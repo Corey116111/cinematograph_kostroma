ef00b710765bfbb6d81d053e77b0dd7196d43a5e
Corey116111 <vitalik.itachi123@yandex.ru>
2025-06-26 20:41:04 +0300

    Notification when the quest is closed

--- app/src/main/java/com/example/intensiv/main_menu_activity.java
+++ app/src/main/java/com/example/intensiv/main_menu_activity.java
@@ -2,16 +2,36 @@ package com.example.intensiv;
 
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.ImageView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class main_menu_activity extends AppCompatActivity
-{
+public class main_menu_activity extends AppCompatActivity {
+
+    private boolean finallyLocked = true; // Флаг блокировки квеста
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         enableFullscreen();
         setContentView(R.layout.main_menu);
+
+        ImageView finallyQuest = findViewById(R.id.finally_quest);
+
+        finallyQuest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (finallyLocked)
+                {
+                    Toast.makeText(main_menu_activity.this,
+                            "Чтобы разблокировать, пройдите квесты по доступным фильмам",
+                            Toast.LENGTH_LONG).show();
+                } else
+                {
+                    // здесь код для разблокированного состояния
+                }
+            }
+        });
     }
 
     private void enableFullscreen() {
@@ -23,5 +43,13 @@ public class main_menu_activity extends AppCompatActivity
                         View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
                         View.SYSTEM_UI_FLAG_FULLSCREEN
         );
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            enableFullscreen();
+        }
     }
 }
